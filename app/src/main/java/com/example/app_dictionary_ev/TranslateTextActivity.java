4bacfd6c6f68dc1645cce6c78fe940d50142c51f
mcuong <huynhmc2804@gmail.com>
2025-04-16 16:29:29 +0700

    lan

--- app/src/main/java/com/example/app_dictionary_ev/TranslateTextActivity.java
+++ app/src/main/java/com/example/app_dictionary_ev/TranslateTextActivity.java
@@ -23,7 +23,13 @@ public class TranslateTextActivity extends AppCompatActivity {
     private boolean boolean_download_language_translation_model = false;
     private EditText edittext_enter_text;
     private TextView textView_translated;
-    private ImageButton button_language_translation;
+    private ImageButton button_language_translation, button_swap;
+
+    private TextView title_Enter, title_Translated;
+
+    private ImageButton button_clear;
+    private boolean isVietnameseToEnglish = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +38,10 @@ public class TranslateTextActivity extends AppCompatActivity {
         edittext_enter_text = findViewById(R.id.editEnterText);
         textView_translated = findViewById(R.id.textTranslated);
         button_language_translation = findViewById(R.id.buttonPlayTrans);
+        button_swap =findViewById(R.id.buttonSwap);
+        title_Enter =findViewById(R.id.tEnter);
+        title_Translated =findViewById(R.id.tTranslated);
+        button_clear = findViewById(R.id.buttonClear);
 
         // Create a translation configuration from English to Vietnamese
         TranslatorOptions options =
@@ -62,7 +72,55 @@ public class TranslateTextActivity extends AppCompatActivity {
 
             }
         });
+        button_swap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isVietnameseToEnglish = !isVietnameseToEnglish;
+                updateTranslator();
 
+                if (isVietnameseToEnglish) {
+                    Toast.makeText(TranslateTextActivity.this, "Dịch từ Tiếng Việt sang Tiếng Anh", Toast.LENGTH_SHORT).show();
+                    title_Enter.setText("Việt");
+                    title_Translated.setText("Anh");
+
+                } else {
+                    Toast.makeText(TranslateTextActivity.this, "Dịch từ Tiếng Anh sang Tiếng Việt", Toast.LENGTH_SHORT).show();
+                    title_Enter.setText("Anh");
+                    title_Translated.setText("Việt");
+                }
+                String tempInput = edittext_enter_text.getText().toString();
+                String tempOutput = textView_translated.getText().toString();
+
+                edittext_enter_text.setText(tempOutput);
+                textView_translated.setText(tempInput);
+            }
+        });
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext_enter_text.setText("");
+                textView_translated.setText("");
+            }
+        });
+    }
+    private void updateTranslator() {
+        TranslatorOptions options;
+
+        if (isVietnameseToEnglish) {
+            options = new TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.VIETNAMESE)
+                    .setTargetLanguage(TranslateLanguage.ENGLISH)
+                    .build();
+        } else {
+            options = new TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.ENGLISH)
+                    .setTargetLanguage(TranslateLanguage.VIETNAMESE)
+                    .build();
+        }
+
+        Translate_from_Vietnamese_to_English = Translation.getClient(options);
+        boolean_download_language_translation_model = false; // reset lại trạng thái
+        Download_language_translation_model(); // tải model mới
     }
     private void Download_language_translation_model(){
         DownloadConditions conditions = new DownloadConditions.Builder()
